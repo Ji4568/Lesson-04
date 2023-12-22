5da7a05915bcb4855d23c94778d90b25c7d9282f
呂佶 <ji8058882@gmail.com>
2023-12-22 21:09:18 +0800

    第一節：資料轉換概念(2)

--- 1.R
+++ 1.R
@@ -5,3 +5,20 @@
 #這份資料是從三軍總醫院生化檢驗值系統截取某10位病患在這段期間內所測得之各式生化值
 dat = read.csv("data3_3.csv", header = TRUE, fileEncoding = 'CP950')
 head(dat, 10)
+
+#資料轉換概念(2)
+#現在這筆資料在轉換時有2個維度，分別是「個案」及「測量時間」，我們在最開始規畫的時候要先把東西盡可能拆離成小部分，最後再用迴圈功能一口氣全部做完
+#一樣，先取得這份資料的基本資訊
+levels.TESTNAME = levels(as.factor(dat[,3]))
+levels.TESTNAME
+
+n.TESTNAME = length(levels.TESTNAME)
+n.TESTNAME
+
+levels.PATNUMBER = levels(as.factor(dat[,1]))
+levels.PATNUMBER
+
+n.PATNUMBER = length(levels.PATNUMBER)
+n.PATNUMBER
+
+
